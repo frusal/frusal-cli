bace3260a71596c4db176a23ea27df9339b7838b
Alex V <alex.vassiliev@gmail.com>
2020-04-26 08:03:25 +1000

    Compiler and linter warnings tidyups.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires, no-undef */
 
 const webpack = require('webpack');
 const shelljs = require('shelljs');
@@ -11,6 +12,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 /** @type {import("webpack").Configuration} */
 const config = {
     entry: './src/main.ts',
+    mode: 'production',
     module: {
         rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }]
     },
@@ -26,8 +28,8 @@ const config = {
         new CleanWebpackPlugin(),
         new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true, entryOnly: true }),
         new CopyPlugin([{ from: "static" }, { from: "README.md" }, { from: "node_modules/@frusal/library-for-node/index.js.LICENSE.txt" }]),
-        compiler => compiler.hooks.done.tap('fs-chmod', stats => shelljs.chmod(755, path.resolve('dist', 'index.js'))), // chmod +x dist/frusal-cli.js
-        compiler => compiler.hooks.done.tap('fs-cp-sub-lic', stats => shelljs.cp("node_modules/@frusal/library-for-node/index.js.LICENSE.txt", "dist")), // pass on license acknowledgements
+        compiler => compiler.hooks.done.tap('fs-chmod', () => shelljs.chmod(755, path.resolve('dist', 'index.js'))), // chmod +x dist/frusal-cli.js
+        compiler => compiler.hooks.done.tap('fs-cp-sub-lic', () => shelljs.cp("node_modules/@frusal/library-for-node/index.js.LICENSE.txt", "dist")), // pass on license acknowledgements
     ],
     output: {
         filename: "index.js"
